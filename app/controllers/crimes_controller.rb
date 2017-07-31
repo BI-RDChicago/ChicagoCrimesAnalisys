228c0d2f8076f61a02ccf0b88086dbe10d0e617b
Guilherme <guigaa@gmail.com>
2017-07-31 16:46:33 -0300

    Variaveis no ETL, correções de typos e melhorias nas consultas

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -16,31 +16,29 @@ class CrimesController < ApplicationController
 
 	def communityfilter
 
-		@community_areas = CommunityArea.all
+		@community_areas = CommunityArea.all.order("description")
 
 	end
 
 	def timefilter
 
-		@community_areas = CommunityArea.all
+		@community_areas = CommunityArea.all.order("description")
 
 	end
 
 	def locationfilter
 
-		@community_areas = CommunityArea.all
+		@community_areas = CommunityArea.all.order("description")
 
 	end
 
 	def communityfilterapply
 
-		@community_areas = CommunityArea.all
-
 		@start = Date.new(*params[:start].values.map(&:to_i))
 		@finish = Date.new(*params[:finish].values.map(&:to_i))
 		@arrest = params[:arrest]
 		@id_source = params[:comm_id]
-		@communityarea_desc = @id_source != "0" ? CommunityArea.find(@id_source) : "All"
+		@communityarea_desc = @id_source != "0" ? CommunityArea.find(@id_source).description : "All"
 
 
 		scope = Crime.select("community_areas.description", "count(0) as qnt").joins(:CommunityArea).group("community_areas.description").order("count(0) desc")
@@ -59,7 +57,7 @@ class CrimesController < ApplicationController
 		@arrest = params[:arrest]
 		@id_source = params[:comm_id]
 		@groupby = params[:groupby]
-		@communityarea_desc = @id_source != "0" ? CommunityArea.find(@id_source) : "All"
+		@communityarea_desc = @id_source != "0" ? CommunityArea.find(@id_source).description : "All"
 		
 		scope = @groupby == "yyyy" ? Crime.select("to_char(date, 'yyyy') as start" , "count(0) as qnt").group("to_char(date, 'yyyy'),to_char(date, 'yyyy')") 
 								   : Crime.select("to_char(date, 'MM/yyyy') as start" , "count(0) as qnt").group("to_char(date, 'MM/yyyy'),to_char(date, 'yyyy/MM')")  
@@ -79,7 +77,7 @@ class CrimesController < ApplicationController
 		@arrest = params[:arrest]
 		@id_source = params[:comm_id]
 		@groupby = params[:groupby]
-		@communityarea_desc = @id_source != "0" ? CommunityArea.find(@id_source) : "All"
+		@communityarea_desc = @id_source != "0" ? CommunityArea.find(@id_source).description : "All"
 		
 		scope = Crime.select("location_description as location" , "count(0) as qnt").group("location_description") 
 
